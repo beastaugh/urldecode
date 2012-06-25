0873a36f0249111641c27af777ac532c2c1212c7
Benedict Eastaugh <benedict@eastaugh.net>
2012-06-26 00:05:20 +0800

    Replace mapM with mapM_ as we discard the result of the mapping.

--- src/urldecode.hs
+++ src/urldecode.hs
@@ -5,5 +5,5 @@ import Network.URI (unEscapeString)
 
 main :: IO ()
 main = do args <- getArgs
-          mapM (putStrLn . unEscapeString) args
+          mapM_ (putStrLn . unEscapeString) args
           return ()
